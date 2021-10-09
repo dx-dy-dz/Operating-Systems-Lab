7f0f9dc8adb5d6cb84fc7993e508be89e9455eaa
Divya Das <72966656+dx-dy-dz@users.noreply.github.com>
2021-10-09 13:30:50 +0530

    Update thread2.c

--- thread2.c
+++ thread2.c
@@ -10,11 +10,14 @@ void *sumOf(void *arg)
     arr = arg;
 
     int n1,n2,sum;
+    
     n1=arr[0];
     n2=arr[1];
+    
     sum = n1+n2;
     printf("Sum => %d + %d = %d\n",n1,n2,sum);
     printf("End of thread\n");
+    
     return NULL;
 }
 
